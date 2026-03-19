d1d231cd936531aac0b2104b2804d0ed5cd3bdf9
qingmingzhang <qingming.zhang@seel.com>
2026-03-19 12:23:50 +0800

    feat: expand QuotesRequest model and add flexible extra_info support

--- SeelWidget/Network/Models/QuotesRequest.swift
+++ SeelWidget/Network/Models/QuotesRequest.swift
@@ -7,14 +7,14 @@ public struct QuotesRequest: Codable, Sendable {
     /// The unique identifier for the merchant within Seel's system
     public var merchantID: String?
     
-    /// The ID of the shopping session
+    /// [Required] The ID of the shopping session
     public var sessionID: String?
     
-    /// The type of device from which user activity originated
+    /// [Required] The type of device from which user activity originated
     /// desktop, mobile or tablet
     public var deviceCategory: String?
     
-    /// The method by which users accessed your website or application
+    /// [Required] The method by which users accessed your website or application
     /// Web, iOS or Android
     public var devicePlatform: String?
     
@@ -24,23 +24,23 @@ public struct QuotesRequest: Codable, Sendable {
     /// The IP address of the client
     public var clientIp: String?
     
-    /// The type of the quote
+    /// [Required] The type of the quote, e.g. "seel-wfp"
     public var type: String?
     
-    /// The default opt-in setting for the quote
+    /// [Required] The default opt-in setting for the quote
     public var isDefaultOn: Bool?
     
-    /// The list of items included in the quote
+    /// [Required] The list of items included in the quote
     public var lineItems: [QuoteLineItem]?
     
-    /// Shipping address information
+    /// [Required] Shipping address information
     public var shippingAddress: QuoteShippingAddress?
     
-    /// Customer information
+    /// [Required] Customer information
     public var customer: QuoteCustomer?
     
-    /// Additional information for the quote
-    public var extraInfo: QuoteExtraInfo?
+    /// Additional information for the quote (open structure, accepts any key-value pairs)
+    public var extraInfo: [String: AnyCodable]?
     
     public init(
         cartID: String? = nil,
@@ -55,7 +55,7 @@ public struct QuotesRequest: Codable, Sendable {
         lineItems: [QuoteLineItem]? = nil,
         shippingAddress: QuoteShippingAddress? = nil,
         customer: QuoteCustomer? = nil,
-        extraInfo: QuoteExtraInfo? = nil
+        extraInfo: [String: AnyCodable]? = nil
     ) {
         self.cartID = cartID
         self.merchantID = merchantID
@@ -80,6 +80,7 @@ public struct QuotesRequest: Codable, Sendable {
         case deviceID = "device_id"
         case deviceCategory = "device_category"
         case devicePlatform = "device_platform"
+        case clientIp = "client_ip"
         case isDefaultOn = "is_default_on"
         case lineItems = "line_items"
         case shippingAddress = "shipping_address"
@@ -89,44 +90,125 @@ public struct QuotesRequest: Codable, Sendable {
 }
 
 public struct QuoteLineItem: Codable, Sendable {
+    /// [Required] The ID of the item
     public var lineItemID: String?
+    /// [Required] The ID of the product
     public var productID: String?
+    /// The ID of the product variant
     public var variantID: String?
+    /// [Required] The title of the product
     public var productTitle: String?
+    /// The description of the product
+    public var productDescription: String?
+    /// The title of the product variant
     public var variantTitle: String?
-    public var price: Double?
-    public var quantity: Int?
-    public var currency: String?
-    public var salesTax: Double?
-    public var requiresShipping: Bool?
-    public var finalPrice: String?
-    public var isFinalSale: Bool?
-    public var allocatedDiscounts: Double?
-    public var category1: String?
-    public var category2: String?
-    public var imageURLs: [String]?
+    /// The sku of the product variant
+    public var sku: String?
+    /// The ID of the seller
+    public var sellerID: String?
+    /// The name of the seller
+    public var sellerName: String?
+    /// The brand name of the product
     public var brandName: String?
+    /// [Required] The quantity of the product
+    public var quantity: Int?
+    /// [Required] The price of the product
+    public var price: Double?
+    /// [Required] The allocated discounts of the product
+    public var allocatedDiscounts: Double?
+    /// [Required] The sales tax of the product
+    public var salesTax: Double?
+    /// The retail price of the product
+    public var retailPrice: Double?
+    /// [Required] The final price of the product
+    public var finalPrice: Double?
+    /// [Required] The currency of the price (ISO 4217), e.g. "USD"
+    public var currency: String?
+    /// [Required] Whether the item requires shipping or not
+    public var requiresShipping: Bool?
+    /// The URL of the product
+    public var productURL: String?
+    /// The URLs of the product images
+    public var imageURLs: [String]?
+    /// [Required] The main category of the product
+    public var category1: String?
+    /// [Required] The sub category of the product
+    public var category2: String?
+    /// The sub category 3 of the product
+    public var category3: String?
+    /// The sub category 4 of the product
+    public var category4: String?
+    /// [Required] Whether the item is final sale or not
+    public var isFinalSale: Bool?
+    /// [Required] The physical condition of the item: "new", "used", or "refurbished"
+    public var condition: String?
+    /// Product attributes (e.g. color, size)
+    public var productAttributes: QuoteProductAttributes?
+    /// Shipping origin information
     public var shippingOrigin: QuoteShippingOrigin?
+    /// Extra information about the item (open structure, accepts any key-value pairs)
+    public var extraInfo: [String: AnyCodable]?
     
-    public init(lineItemID: String? = nil, productID: String? = nil, variantID: String? = nil, productTitle: String? = nil, variantTitle: String? = nil, price: Double? = nil, quantity: Int? = nil, currency: String? = nil, salesTax: Double? = nil, requiresShipping: Bool? = nil, finalPrice: String? = nil, isFinalSale: Bool? = nil, allocatedDiscounts: Double? = nil, category1: String? = nil, category2: String? = nil, imageURLs: [String]? = nil, brandName: String? = nil, shippingOrigin: QuoteShippingOrigin? = nil) {
+    public init(
+        lineItemID: String? = nil,
+        productID: String? = nil,
+        variantID: String? = nil,
+        productTitle: String? = nil,
+        productDescription: String? = nil,
+        variantTitle: String? = nil,
+        sku: String? = nil,
+        sellerID: String? = nil,
+        sellerName: String? = nil,
+        brandName: String? = nil,
+        quantity: Int? = nil,
+        price: Double? = nil,
+        allocatedDiscounts: Double? = nil,
+        salesTax: Double? = nil,
+        retailPrice: Double? = nil,
+        finalPrice: Double? = nil,
+        currency: String? = nil,
+        requiresShipping: Bool? = nil,
+        productURL: String? = nil,
+        imageURLs: [String]? = nil,
+        category1: String? = nil,
+        category2: String? = nil,
+        category3: String? = nil,
+        category4: String? = nil,
+        isFinalSale: Bool? = nil,
+        condition: String? = nil,
+        productAttributes: QuoteProductAttributes? = nil,
+        shippingOrigin: QuoteShippingOrigin? = nil,
+        extraInfo: [String: AnyCodable]? = nil
+    ) {
         self.lineItemID = lineItemID
         self.productID = productID
         self.variantID = variantID
         self.productTitle = productTitle
+        self.productDescription = productDescription
         self.variantTitle = variantTitle
-        self.price = price
+        self.sku = sku
+        self.sellerID = sellerID
+        self.sellerName = sellerName
+        self.brandName = brandName
         self.quantity = quantity
-        self.currency = currency
-        self.salesTax = salesTax
-        self.requiresShipping = requiresShipping
-        self.finalPrice = finalPrice
-        self.isFinalSale = isFinalSale
+        self.price = price
         self.allocatedDiscounts = allocatedDiscounts
+        self.salesTax = salesTax
+        self.retailPrice = retailPrice
+        self.finalPrice = finalPrice
+        self.currency = currency
+        self.requiresShipping = requiresShipping
+        self.productURL = productURL
+        self.imageURLs = imageURLs
         self.category1 = category1
         self.category2 = category2
-        self.imageURLs = imageURLs
-        self.brandName = brandName
+        self.category3 = category3
+        self.category4 = category4
+        self.isFinalSale = isFinalSale
+        self.condition = condition
+        self.productAttributes = productAttributes
         self.shippingOrigin = shippingOrigin
+        self.extraInfo = extraInfo
     }
 
     enum CodingKeys: String, CodingKey {
@@ -134,38 +216,102 @@ public struct QuoteLineItem: Codable, Sendable {
         case productID = "product_id"
         case variantID = "variant_id"
         case productTitle = "product_title"
+        case productDescription = "product_description"
         case variantTitle = "variant_title"
-        case price, quantity, currency
-        case salesTax = "sales_tax"
-        case requiresShipping = "requires_shipping"
-        case finalPrice = "final_price"
-        case isFinalSale = "is_final_sale"
+        case sku
+        case sellerID = "seller_id"
+        case sellerName = "seller_name"
+        case brandName = "brand_name"
+        case quantity, price
         case allocatedDiscounts = "allocated_discounts"
+        case salesTax = "sales_tax"
+        case retailPrice = "retail_price"
+        case finalPrice = "final_price"
+        case currency
+        case requiresShipping = "requires_shipping"
+        case productURL = "product_url"
+        case imageURLs = "image_urls"
         case category1 = "category_1"
         case category2 = "category_2"
-        case imageURLs = "image_urls"
-        case brandName = "brand_name"
+        case category3 = "category_3"
+        case category4 = "category_4"
+        case isFinalSale = "is_final_sale"
+        case condition
+        case productAttributes = "product_attributes"
         case shippingOrigin = "shipping_origin"
+        case extraInfo = "extra_info"
+    }
+}
+
+public struct QuoteProductAttributes: Codable, Sendable {
+    /// The color of the product
+    public var color: String?
+    /// The size of the product
+    public var size: String?
+    
+    public init(color: String? = nil, size: String? = nil) {
+        self.color = color
+        self.size = size
     }
 }
 
 public struct QuoteShippingOrigin: Codable, Sendable {
+    /// [Required] ISO 3166-1 alpha-2 country code
     public var country: String?
+    public var address1: String?
+    public var address2: String?
+    public var city: String?
+    public var state: String?
+    public var zipcode: String?
     
-    public init(country: String? = nil) {
+    public init(
+        country: String? = nil,
+        address1: String? = nil,
+        address2: String? = nil,
+        city: String? = nil,
+        state: String? = nil,
+        zipcode: String? = nil
+    ) {
         self.country = country
+        self.address1 = address1
+        self.address2 = address2
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case country
+        case address1 = "address_1"
+        case address2 = "address_2"
+        case city, state, zipcode
     }
 }
 
 public struct QuoteShippingAddress: Codable, Sendable {
+    /// [Required] The first line of the shipping address
     public var address1: String?
+    /// The second line of the shipping address
+    public var address2: String?
+    /// [Required] The city of the shipping address
     public var city: String?
+    /// [Required] The state or province code of the shipping address
     public var state: String?
+    /// [Required] The zipcode of the shipping address
     public var zipcode: String?
+    /// [Required] ISO 3166-1 alpha-2 country code
     public var country: String?
     
-    public init(address1: String? = nil, city: String? = nil, state: String? = nil, zipcode: String? = nil, country: String? = nil) {
+    public init(
+        address1: String? = nil,
+        address2: String? = nil,
+        city: String? = nil,
+        state: String? = nil,
+        zipcode: String? = nil,
+        country: String? = nil
+    ) {
         self.address1 = address1
+        self.address2 = address2
         self.city = city
         self.state = state
         self.zipcode = zipcode
@@ -174,23 +320,39 @@ public struct QuoteShippingAddress: Codable, Sendable {
 
     enum CodingKeys: String, CodingKey {
         case address1 = "address_1"
+        case address2 = "address_2"
         case city, state, zipcode, country
     }
 }
 
 public struct QuoteCustomer: Codable, Sendable {
+    /// [Required] The unique identifier for the customer
     public var customerID: String?
+    /// The first name of the customer
     public var firstName: String?
+    /// The last name of the customer
     public var lastName: String?
+    /// [Required] The email address of the customer
     public var email: String?
+    /// The phone number of the customer
     public var phone: String?
+    /// Extra information about the customer (open structure, accepts any key-value pairs)
+    public var extraInfo: [String: AnyCodable]?
     
-    public init(customerID: String? = nil, firstName: String? = nil, lastName: String? = nil, email: String? = nil, phone: String? = nil) {
+    public init(
+        customerID: String? = nil,
+        firstName: String? = nil,
+        lastName: String? = nil,
+        email: String? = nil,
+        phone: String? = nil,
+        extraInfo: [String: AnyCodable]? = nil
+    ) {
         self.customerID = customerID
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
         self.phone = phone
+        self.extraInfo = extraInfo
     }
 
     enum CodingKeys: String, CodingKey {
@@ -198,17 +360,6 @@ public struct QuoteCustomer: Codable, Sendable {
         case firstName = "first_name"
         case lastName = "last_name"
         case email, phone
-    }
-}
-
-public struct QuoteExtraInfo: Codable, Sendable {
-    public var shippingFee: Double?
-    
-    public init(shippingFee: Double? = nil) {
-        self.shippingFee = shippingFee
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case shippingFee = "shipping_fee"
+        case extraInfo = "extra_info"
     }
 }
